648a81f057cd7c2ce36c3940710bd06de5687834
Tamjid-Mostafa <tamjid430@gmail.com>
2023-01-13 01:02:29 +0600

    Custom css changes

--- src/Pages/Home/BestDeveloper/BestDeveloper.js
+++ src/Pages/Home/BestDeveloper/BestDeveloper.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SectionDivider from '../../../components/Buttons/SectionDivider';
 
 const BestDeveloper = () => {
     return (
         <div>
+            <SectionDivider/>
             <p>Best developer</p>
         </div>
     );
