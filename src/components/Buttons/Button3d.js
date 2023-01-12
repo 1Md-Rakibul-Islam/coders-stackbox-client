3506671de3ebf0463554312d3af6f51cd087061d
Tamjid-Mostafa <tamjid430@gmail.com>
2023-01-12 11:44:33 +0600

    Customized Button

--- src/components/Buttons/Button3d.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button3d = ({ styles, children }) => (
-  <button className={`py-4 px-6 font-poppins font-medium text-[18px] text-white bg-blue-gradient rounded-[10px] outline-none custom-btn btn-3d ${styles}`}>
-    {children}
-  </button>
-);
-
-export default Button3d;
