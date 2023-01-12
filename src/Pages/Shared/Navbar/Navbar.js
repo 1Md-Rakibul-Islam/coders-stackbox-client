d35f4dfffc2a6ee0b240f88ac183ed649f240ea6
0178811jes <jesminara8653@gmail.com>
2023-01-12 17:23:44 +0600

    navbar some change

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     let [open, setOpen] =useState(false);
 
     return (
-        <div className='shadow-md w-full fixed top-0 left-0 '>
+        <div className='shadow-md w-full fixed z-10 top-0 left-0 '>
             <div className="md:flex justify-between py-4 bg-white md:px-10 px-7">
                 <div className='font-bold text-2xl cursor-pointer flex items-center font-[poppins] text-gray-800'>
                     <span className='text-3xl text-indigo-600 mr-1 pt-2'>
@@ -16,12 +16,12 @@ const Navbar = () => {
                     Coddinglab
                 </div>
                 <div onClick={() =>setOpen(!open)} className='text-3xl absolute right-8 top-6
-                cursor-pointer md:hidden
+                cursor-pointer md:hidden block
                 '>
                 <ion-icon name={open ? 'close':'menu'}></ion-icon>
                 </div>
-                <ul className={`md:flex md:items-center md:pb-0
-                pb-12 absolute md:static bg-white md:z-auto z-[-1]
+                <ul className={`lg:flex md:items-center md:pb-0
+                pb-12 absolute md:static bg-white z-10 
                 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in
                 ${open ?'top-20 opacity-100':'top-[-490px]'} `}>
                         <li><Link className="ml-8" to='/'>Home</Link></li>
