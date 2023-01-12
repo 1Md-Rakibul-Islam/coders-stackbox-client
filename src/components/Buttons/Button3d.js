0b74e4f168741c370690a4ef1bfcdad78c81a3cd
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2023-01-12 11:03:24 +0600

    commit for contact us

--- src/components/Buttons/Button3d.js
+++ src/components/Buttons/Button3d.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Button3d = ({ styles, children }) => (
-  <button className={`py-4 px-6 font-poppins font-medium text-[18px] text-white bg-blue-gradient rounded-[10px] outline-none btn-3d ${styles}`}>
+  <button
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-white bg-blue-gradient rounded-[10px] outline-none btn-3d ${styles}`}
+  >
     {children}
   </button>
 );
