b898fd91505eb4cf1b2dca486c9234eac027a68c
Tamjid-Mostafa <tamjid430@gmail.com>
2023-01-12 10:52:05 +0600

    added a custom button component and changed the bg

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Shared/Footer/Footer';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import styles from "../style";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+  return (
+    <div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
-    );
+      </div>
+      <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Outlet />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
