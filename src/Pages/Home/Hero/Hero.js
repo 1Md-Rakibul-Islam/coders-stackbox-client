50965101480807e17887e99b0b26eb754e219acb
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2023-01-12 17:16:49 +0600

    second commit from rakibul

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <header>
             <div>
                 <div>
-                    <h2>Build your Front-End Web Application esay way. Coders StackBox is the best place to build, test, and discover front-end code.</h2>
+                    <h2 className='text-center text-4xl'>Build your Front-End Web Application esay way. Coders StackBox is the best place to build, test, and discover front-end code.</h2>
                     <div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet provident quae exercitationem vitae officiis rem deserunt dolor quos accusantium! Id, quo tenetur. Sed explicabo atque ratione, fugit pariatur dolore dicta voluptatibus qui commodi autem minima alias, enim eligendi dolorum fugiat recusandae dignissimos expedita vero! Corporis expedita sint tenetur consequatur, quibusdam rem non unde debitis laborum, neque nihil magnam voluptatibus iure.</p>
                         
