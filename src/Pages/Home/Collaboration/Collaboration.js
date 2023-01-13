c916ec83f2ccfb5b21207b44a8bf8684eb77e225
Tamjid-Mostafa <tamjid430@gmail.com>
2023-01-13 20:58:10 +0600

    Collaboration Section Customized

--- src/Pages/Home/Collaboration/Collaboration.js
+++ src/Pages/Home/Collaboration/Collaboration.js
@@ -7,7 +7,7 @@ const Collaboration = () => {
   return (
     <div className={`${layout.sectionCol}`}>
       <div className={`${layout.sectionInfo} text-center`}>
-        <div className={`${styles.heading2} `}>Best Developers</div>
+        <div className={`${styles.heading2} `}>Collaborations</div>
         <div className="grid gap-7 md:grid-cols-2 lg:grid-cols-3 mx-auto my-5">
           {collabs.map((collab) => (
             <CollabsCard key={collab.id} props={collab}></CollabsCard>
--- src/components/Cards/CollabsCard.js
+++ src/components/Cards/CollabsCard.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import styles from '../../style'
+import React from "react";
+import styles from "../../style";
 
-const CollabsCard = ({props}) => {
+const CollabsCard = ({ props }) => {
   return (
-    <div className="relative rounded-lg overflow-hidden">
-      <div className={`${styles.flexStart} gap-4 m-5`}>
+    <div className="relative rounded-lg overflow-hidden glassmorphism">
+      <div className={`flex flex-col justify-center items-center`}>
         {props.icon}
-        <div>
-          <div className={` ${styles.heading2}`}>{props.name}</div>
-          <div className={`text-dimGreen`}>{props.title}</div>
+
+        <div className={`${styles.flexCenter}`}>
+          <div className={` ${styles.heading3}`}>{props.title}</div>
         </div>
       </div>
-
-        <div className="flex justify-center px-4">
+      <div className="flex justify-center px-4">
         <div
           className={`bg-gradient-to-r from-accent to-secondary w-full `}
           style={{ height: "2px" }}
         />
-        </div>
+      </div>
+
       <div className="p-4">
         <div className={`${styles.paragraph}`}>{props.details}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollabsCard
+export default CollabsCard;
