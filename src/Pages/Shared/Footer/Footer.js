a30affca5a7f79704e7a5af10bc0e0af15b29f95
0178811jes <jesminara8653@gmail.com>
2023-01-13 16:21:15 +0600

    footer changes

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,13 +6,14 @@ import styles from '../../../style';
 
 const Footer = () => {
     return (
-        
-            <footer className={`${styles.boxWidth} mx-auto footer p-10 text-white relative flex justify-between `}>
+
+        <footer className={`${styles.boxWidth} mx-auto relative text-center `}>
             <div className="footer-gradient" />
+            <div className='w-full'>
                 <div>
-                    
+
                     <ul className='lg:flex md:flex sm:flex items-center mt-10 text-bold justify-around text-white'>
-                    <li><Link to=''>Home</Link></li>
+                        <li><Link to=''>Home</Link></li>
                         <li><Link to=''>About Us</Link></li>
                         <li><Link to=''><span className="text-white text-bold text-2xl">Services</span></Link></li>
                         <li><Link to=''>Documentation</Link></li>
@@ -20,21 +21,23 @@ const Footer = () => {
                         {/* <li><Link to=''>Login</Link></li>
                         <li><Link to="">Sign Up</Link></li> */}
                     </ul>
+                    <div className='mt-10 bg-gradient-to-r from-accent to-secondary ' style={{ height: "2px" }} />
                 </div>
-                <hr className='mt-10'/>
-                <div className='text-white'>
-                   <div className='mt-5 mb-5 pt-10'>
-                   <a className='mr-5 rounded-full ...' href=""><ion-icon name="logo-github"></ion-icon></a>
-                   <a className='mr-5' href=""><ion-icon name="logo-linkedin"></ion-icon></a>
-                   <a className='mr-5' href=""><ion-icon name="logo-facebook"></ion-icon></a>
-                   <a className='mr-5' href=""><ion-icon name="logo-ionic"></ion-icon></a>
-                   </div>
-                   <div>
-                   <a className='mt-5 mb-5 pt-10' href=""><ion-icon name="logo-edge"></ion-icon>  2010-2020 Privacy-Terms</a>
-                   </div>
-                </div> 
-            </footer>
-        
+                <div className='text-white text-center'>
+                    <div className='mt-5 mb-5 pt-10'>
+                        <a className='mr-5 rounded-full ...' href=""><ion-icon name="logo-github"></ion-icon></a>
+                        <a className='mr-5' href=""><ion-icon name="logo-linkedin"></ion-icon></a>
+                        <a className='mr-5' href=""><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a className='mr-5' href=""><ion-icon name="logo-ionic"></ion-icon></a>
+
+                    </div>
+                    <div>
+                        <a className='mt-5 mb-5 pt-10' href=""><ion-icon name="logo-edge"></ion-icon>  2010-2020 Privacy-Terms</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
     );
 };
 
