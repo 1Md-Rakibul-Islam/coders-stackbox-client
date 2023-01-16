f39de07c72f1c9557c9143bba2808aa8b1577b9e
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2023-01-16 16:56:41 +0600

    community page started

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,23 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import CommunitySupport from "../../Pages/CommunitySupport/CommunitySupport";
 import Home from "../../Pages/Home/Home/Home";
-import Services from "../../Pages/Home/Services/Services";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            }
-        ]
-    },
-    {
-        path:'*',
-        element: <div className="texl-3xl text-center">Page is not available</div>
-    }
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/community",
+        element: <CommunitySupport></CommunitySupport>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <div className="texl-3xl text-center">Page is not available</div>,
+  },
 ]);
 
-export default router
+export default router;
