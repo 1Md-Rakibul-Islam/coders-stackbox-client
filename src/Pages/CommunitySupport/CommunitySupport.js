5fc2aa7438f260acefedb47443bca7eff5d989f7
xbayazid <bayazid9006@gmail.com>
2023-01-18 19:53:10 +0600

    support page completed

--- src/Pages/CommunitySupport/CommunitySupport.js
+++ src/Pages/CommunitySupport/CommunitySupport.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactUs from '../Home/ConactUs/ContactUs';
 import FAQ from "./subsections/FAQ/FAQ";
+import HelpCategories from './subsections/HelpCategories/HelpCategories';
 
 const CommunitySupport = () => {
   return (
     <div>
+      <HelpCategories></HelpCategories>
       <FAQ></FAQ>
       <ContactUs></ContactUs>
     </div>
