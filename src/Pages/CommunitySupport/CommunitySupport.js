b158d114a68fd6aafa85a72f19b90a9c5552caa4
xbayazid <bayazid9006@gmail.com>
2023-01-18 18:37:57 +0600

    community section added

--- src/Pages/CommunitySupport/CommunitySupport.js
+++ src/Pages/CommunitySupport/CommunitySupport.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactUs from '../Home/ConactUs/ContactUs';
 import FAQ from "./subsections/FAQ/FAQ";
 
 const CommunitySupport = () => {
   return (
     <div>
       <FAQ></FAQ>
+      <ContactUs></ContactUs>
     </div>
   );
 };
